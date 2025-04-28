54f022ee8abd7a6ccba4177336fadc77872a2109
SachinXStinger <sachinsinghsikarwal05@gmail.com>
2025-04-28 23:35:56 +0530

    minor changes

--- Backend/src/main/java/com/example/WeatherApp/WeatherAppApplication.java
+++ Backend/src/main/java/com/example/WeatherApp/WeatherAppApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class WeatherAppApplication {
 
+	// This is the main file for weather app
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherAppApplication.class, args);
 	}
